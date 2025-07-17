94ae96480aac6c848b709d952b594a83732f2b50
Jonas da Silva <phoenixr41414141@gmail.com>
2025-07-17 20:20:52 +0200

    ✨ Update UI library

--- src/ui/lib/mod.ts
+++ src/ui/lib/mod.ts
@@ -1,1 +1,7 @@
-export const black = "§0"
+/** Color code that represents #000000. */
+export const black = "§0";
+
+/** Color code that represents  #0000AA. */
+export const dark_blue = "§1";
+
+// TODO: rest

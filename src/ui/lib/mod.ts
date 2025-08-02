6fd51f86985cb665626328b4225120f45e6fdd0c
Jonas da Silva <phoenixr41414141@gmail.com>
2025-08-03 00:50:53 +0200

    ✨ Implement style function and tests

--- src/ui/lib/mod.ts
+++ src/ui/lib/mod.ts
@@ -2,42 +2,81 @@
 //       either enable some style or not included in the output if invalid.
 
 import * as xml from "@melvdouc/xml-parser";
+import * as html from "@std/html";
 
 export const codes = {
-  /** Color code that represents #000000. */
-  black: "§0",
+  colors: {
+    /** Color code that represents #000000. */
+    black: "§0",
 
-  /** Color code that represents  #0000AA. */
-  dark_blue: "§1",
+    /** Color code that represents  #0000AA. */
+    darkBlue: "§1",
 
-  dark_green: "§2",
+    darkGreen: "§2",
 
-  dark_aqua: "§3",
+    darkAqua: "§3",
 
-  dark_red: "§4",
+    darkRed: "§4",
 
-  dark_purple: "§5",
+    darkPurple: "§5",
 
-  gold: "§6",
+    gold: "§6",
 
-  gray: "§7",
+    gray: "§7",
 
-  dark_gray: "§8",
+    darkGray: "§8",
 
-  blue: "§9",
+    blue: "§9",
 
-  green: "§a",
+    green: "§a",
 
-  aqua: "§b",
+    aqua: "§b",
 
-  red: "§c",
+    red: "§c",
 
-  light_purple: "§d",
-  // TODO: rest
+    lightPurple: "§d",
+
+    yellow: "§e",
+
+    white: "§f",
+
+    minecoinGold: "§g",
+
+    materialQuartz: "§h",
+
+    materialIron: "§i",
+
+    materialNetherite: "§j",
+
+    materialRedstone: "§m",
+
+    materialCopper: "§n",
+
+    materialGold: "§p",
+
+    materialEmerald: "§q",
+
+    materialDiamond: "§s",
+
+    materialLapis: "§t",
+
+    materialAmethyst: "§u",
+
+    materialResin: "§v",
+  },
+  formatting: {
+    obfuscated: "§k",
+
+    bold: "§l",
+
+    italic: "§o",
+
+    reset: "§r",
+  },
 } as const;
 
 export function strip(text: string) {
-  return text.replaceAll(/§./, "");
+  return text.replaceAll(/§./g, "");
 }
 
 /**
@@ -46,7 +85,42 @@ export function strip(text: string) {
  * @example
  * style`foo <red>bar <bold>baz</bold> blah</red> blup`
  */
-export function style(template: TemplateStringsArray, ...params: string[]) {
-  // TODO: escape interpolated values
-  return "TODO";
+export function style(template: TemplateStringsArray, ...params: unknown[]) {
+  let sanitized = "";
+  for (const i in template) {
+    const left = template[i];
+    const right = html.escape(strip(`${params[i] ?? ""}`));
+    sanitized += `${left}${right}`;
+  }
+
+  return html.unescape(transformXML(xml.parse(sanitized), []));
+}
+
+function transformXML(nodes: xml.XmlNode[], restore: (string | undefined)[]) {
+  let result = "";
+  console.debug({ nodes });
+  let index = 0;
+  for (const node of nodes) {
+    const lastTagNode = index + 1 == nodes.filter((n) =>
+      n.kind === "REGULAR_TAG_NODE"
+    ).length;
+    if (node.kind === "REGULAR_TAG_NODE") {
+      index++;
+      const codesWithLowercasedKeys = Object.fromEntries(
+        Object.entries({...codes.colors, ...codes.formatting}).map(([key, value]) => [key.toLowerCase(), value]),
+      );
+      const code = codesWithLowercasedKeys[node.tagName.toLowerCase()];
+      result += code ?? "";
+      restore.push(code);
+      result += transformXML(node.children, restore);
+      if (lastTagNode) {
+        restore.pop();
+        result += `§r${restore.filter((x) => x !== undefined).join("")}`;
+      }
+    }
+    if (node.kind === "TEXT_NODE") {
+      result += node.value;
+    }
+  }
+  return result;
 }
---
+++ src/ui/tests/style_test.ts
@@ -0,0 +1,43 @@
+import { assertEquals, assertThrows } from "jsr:@std/assert";
+import { style } from "@bedrock-ws/ui";
+
+Deno.test("basic text", () => {
+  assertEquals(style`Hello World`, "Hello World");
+});
+
+Deno.test("one color", () => {
+  assertEquals(style`<red>Hello World</red>`, "§cHello World§r");
+});
+
+Deno.test("nested styling", () => {
+  assertEquals(style`<red>A<bold>B</bold>C</red>`, "§cA§lB§r§cC§r");
+});
+
+Deno.test("deeply nested styling", () => {
+  assertEquals(
+    style`A<red>B<bold>C<italic>D</italic>E</bold>F</red>G`,
+    "A§cB§lC§oD§r§c§lE§r§cF§rG",
+  );
+});
+
+Deno.test("reset node", () => {
+  assertEquals(style`<red>A<reset>B</reset>C</red>`, "§cA§rB§r§cC§r");
+});
+
+Deno.test("invalid style", () => {
+  assertEquals(style`<blah>A</blah>`, "A§r");
+});
+
+Deno.test("overlap", () => {
+  assertThrows(() => {
+    style`<red>A<bold>B</red>C</bold>`;
+  });
+});
+
+Deno.test("different casing", () => {
+  assertEquals(style`<rED>A</rED>`, "§cA§r");
+});
+
+Deno.test("injection", () => {
+  assertEquals(style`${"<red>"}A${"</red>"}`, "<red>A</red>");
+});
